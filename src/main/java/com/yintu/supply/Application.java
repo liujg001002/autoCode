bbb245955927ba1181bb1b21ec3c3a9ea4931f0e
liujianguo <1149149610@qq.com>
2020-02-18 10:13:21 +0800

    modify configuration

--- src/main/java/com/yintu/supply/Application.java
+++ src/main/java/com/yintu/supply/Application.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-// SpringBoot默认会自动配置数据库，如果业务不需要，就要手动禁用数据库自动配置
 @SpringBootApplication
-//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
-//		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @EnableAsync
 @PropertySource("classpath:application.yml")
 public class Application {
